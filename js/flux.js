382d6c0650422ed7c5acc051ac8569b5c289b52d
nicklally <nicklally@gmail.com>
2016-04-22 15:23:38 -0500

    2

--- js/flux.js
+++ js/flux.js
@@ -20,10 +20,17 @@ function setup() {
 }
 
 function draw() {
+	background(255);
+	checkOver(mouseX,mouseY);
+	if(mode == 0){
+		for(var i = 0; i < boxes.length; i++){
+			boxes[i].addLine(mouseX,mouseY);
+		}
+	}
 	if(mode == 1){
 		moveBox(mouseX,mouseY);
-		recalcBoxes();
 	}
+	recalcBoxes();
 }
 
 function Box(bX, bY, bW, bH, name, mag, id){
@@ -35,20 +42,78 @@ function Box(bX, bY, bW, bH, name, mag, id){
 	this.mag = mag; 
 	this.id = id; 
 	this.fillC = 200;
+	this.locked = false;
+	this.over = false;
+	this.difx = 0; //to calculate dragging
+	this.dify = 0;
+	this.lineMode = false;
+	this.lineX1 = 0;
+	this.lineY1 = 0;
+	this.lineX2 = 0; 
+	this.lineY2 = 0;
+	this.lineLocked = false;
 	
 	this.display = function(){
 		fill(this.fillC);
 		rect(this.bX, this.bY, this.bW, this.bH);
+		line(this.lineX1,this.lineY1,this.lineX2,this.lineY2);
 	};
+	
+	this.checkOver = function(x,y){
+		if(x > this.bX && x < this.bX+this.bW && y > this.bY && y < this.bY + this.bH){
+			this.over = true;
+		} else {
+			this.over = false;
+		}		
+	}
+	
+	this.move = function(x,y){
+		if(this.over){
+			this.fillC = 255;
+		} else {
+			this.fillC = 200;
+		}	
+		//help from http://processingjs.org/learning/basic/mousefunctions/
+		if(this.over && mouseIsPressed && !this.locked){
+			this.locked = true;
+			this.difx = x - this.bX;
+			this.dify = y - this.bY;
+			//console.log(this.difx + ' ' + this.dify);
+		} 
+		if(!mouseIsPressed) {
+			this.locked = false;
+		}	
+	}
+	
+	this.addLine = function(x,y){
+		if(this.over && !this.lineMode && mouseIsPressed){
+			this.lineX1 = x;
+			this.lineY1 = y; 
+			this.lineLocked = true;
+			this.lineMode = true;
+		} 
+		
+		if(this.lineLocked && this.lineMode && mouseIsPressed){
+			this.lineX2 = x; 
+			this.lineY2 = y;
+		}
+		if(!mouseIsPressed){
+			this.lineLocked = false;
+			this.lineMode = false;
+		}
+		
+	}
 }
 
 function moveBox(x,y){
 	for(var i = 0; i < boxes.length; i++){
-		if(x > boxes[i].bX && x < boxes[i].bX+boxes[i].bW && y > boxes[i].bY && y < boxes[i].bY + boxes[i].bH){
-			boxes[i].fillC = 255;
-		} else {
-			boxes[i].fillC = 200;
-		}	
+		boxes[i].move(x,y);
+	}
+}
+
+function checkOver(x,y){
+	for(var i = 0; i < boxes.length; i++){
+		boxes[i].checkOver(x,y);
 	}
 }
 
@@ -60,7 +125,23 @@ function mouseReleased(){
 	}*/
 }
 
+function mouseDragged(){
+	if(mode == 1){
+		for(var i = 0; i < boxes.length; i++){
+			if(boxes[i].locked){
+				var nx = mouseX - boxes[i].difx;
+				var ny = mouseY - boxes[i].dify;
+				boxes[i].bX = nx;
+				boxes[i].bY = ny;
+				moveInputs(nx,ny,"#mag_" + i, "#label_" + i);
+			}
+		}
+	}
+	recalcBoxes();
+}
+
 function recalcBoxes(){
+	background(255);
 	for(var i = 0; i < boxes.length; i++){
 		boxes[i].display();
 	}
@@ -89,6 +170,17 @@ function makeInputs(x,y,id){
 
 }
 
+function moveInputs(x,y,id1,id2){
+	x = x + 0.5*boxW;
+	y = y + 0.5*boxH;
+	var inputFocus = select(id1);
+	inputFocus.position(x-25,y);
+	
+	var inputFocus2 = select(id2);
+	inputFocus2.position(x-25,y-25);
+	
+}
+
 function makeBox(x,y){
 	x = x - 0.5*boxW;
 	y = y - 0.5*boxH;
@@ -101,7 +193,7 @@ function makeBox(x,y){
 			}
 		}
 		//if no overlap, make new box
-		if(!overlap && !lock){
+		if(!overlap){
 			x = x+0.5*boxW;
 			y = y+0.5*boxH;
 			append(boxes, new Box(x, y,boxW,boxH,' ', 0, boxes.length));
